1b527ceb5a7889766e9c1270a8678582c487bad7
Yingjie Ma <47992202+ym1014@users.noreply.github.com>
2021-05-07 17:31:06 -0400

    all images

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,16 @@ export default function Home() {
       <Head>
         <title>Panimate</title>
       </Head>
-      <img src="/people.svg" alt="" />
+
+      <img src="/people.svg" alt="" width="200" height="250" />
+      <img src="/pinkflower.svg" alt="" width="200" height="250" />
+      <img src="/blueflower.svg" alt="" width="200" height="250" />
+      <img src="/redbutton.svg" alt="" width="100" height="250" />
+      <img src="/bluebutton.svg" alt="" width="100" height="250" />
+      <img src="/Vector 4.svg" alt="" width="100" height="250" />
+      <img src="/Vector 6.svg" alt="" width="100" height="250" />
+      <img src="/Vector 8.svg" alt="" width="100" height="150" />
+      <img src="/Vector 7.svg" alt="" width="100" height="150" />
     </Layout>
   );
 }
